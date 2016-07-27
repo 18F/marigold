c06093bfa5001f391e3e144bfb19b5ed973cb3a3
Julia Elman <julia.elman@gsa.gov>
2016-07-27 14:28:47 -0400

    Updated fractal.js to point to correct assets folder.

--- fractal.js
+++ fractal.js
@@ -9,5 +9,5 @@ fractal.components.set('default.preview', '@preview');
 
 fractal.docs.set('path', __dirname + '/src/docs');
 
-fractal.web.set('static.path', __dirname + '/assets');
+fractal.web.set('static.path', __dirname + '/src/assets');
 fractal.web.set('builder.dest', __dirname + '/build');
