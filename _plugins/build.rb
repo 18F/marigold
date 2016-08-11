c3f3c69e9d3b076ae5cd6dc7e154b06ef3ddc359
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2016-08-10 23:44:26 -0400

    Run plugin on all federalist builds

--- _plugins/build.rb
+++ _plugins/build.rb
@@ -1,7 +1,7 @@
 # these are the branches for which we build production assets
 branches = ['master', 'staging']
 
-if branches.include?(ENV['BRANCH'])
+if ENV['BRANCH']
   puts '[build.rb] building for production'
   ENV['NODE_ENV'] = 'production'
   `npm install && npm run build > build.log`
