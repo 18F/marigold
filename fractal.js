50b7274c119adf334f84a266e983ad2ffa5012bc
Julia Elman <julia.elman@gsa.gov>
2016-10-26 04:09:43 -0400

    Adding build path to root level assets dir.

--- fractal.js
+++ fractal.js
@@ -10,4 +10,5 @@ fractal.components.set('default.preview', '@preview');
 fractal.docs.set('path', __dirname + '/src/docs');
 
 fractal.web.set('static.path', __dirname + '/src/assets');
+fractal.web.set('static.mount', 'assets');
 fractal.web.set('builder.dest', __dirname + '/build');
