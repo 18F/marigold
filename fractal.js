f019c3cca01525d4bdbcea4b2db607aa312c85b4
Shawn Allen <shawn.allen@gsa.gov>
2016-10-31 12:44:21 -0700

    use nunjucks as the templating engine

--- fractal.js
+++ fractal.js
@@ -4,11 +4,20 @@ const fractal = module.exports = require('@frctl/fractal').create();
 
 fractal.set('project.title', 'Marigold');
 
-fractal.components.set('path', __dirname + '/src/components');
+// use Nunjucks as the templating engine
+fractal.components.engine(require('@frctl/nunjucks')({
+  filters: require('./lib/template-filters')
+}));
+
+const src = __dirname + '/src';
+
+fractal.components.set('ext', '.html');
+fractal.components.set('path', src + '/components');
 fractal.components.set('default.preview', '@preview');
 
-fractal.docs.set('path', __dirname + '/src/docs');
+fractal.docs.set('path', src + '/docs');
 
-fractal.web.set('static.path', __dirname + '/src/assets');
+fractal.web.set('static.path', src + '/assets');
 fractal.web.set('static.mount', 'assets');
+
 fractal.web.set('builder.dest', __dirname + '/build');
