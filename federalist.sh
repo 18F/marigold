24c1d2ee2b5d78623b3d5a2312a053610d32a5e7
Shawn Allen <shawnbot@users.noreply.github.com>
2016-10-31 13:39:08 -0700

    try without nvm

--- federalist.sh
+++ federalist.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+# [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 npm install 2>&1 | tee build.log
 npm run build 2>&1 | tee -a build.log
 cp -rf build/* .
