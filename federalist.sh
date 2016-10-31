d146b0c1591e16ee0e8c1f12016c39ac37c25f65
Shawn Allen <shawnbot@users.noreply.github.com>
2016-10-31 13:51:28 -0700

    unset PREFIX; bring back nvm
    see 18F/federalist#570 for more info

--- federalist.sh
+++ federalist.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
-# [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+unset PREFIX
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 npm install 2>&1 | tee build.log
 npm run build 2>&1 | tee -a build.log
 cp -rf build/* .
