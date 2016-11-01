7ae52bafceafc9af003bda1869d48eec911d90ac
Shawn Allen <shawn.allen@gsa.gov>
2016-11-01 12:44:43 -0700

    add sass conversion to fractal.js

--- fractal.js
+++ fractal.js
@@ -2,14 +2,14 @@
 
 const fractal = module.exports = require('@frctl/fractal').create();
 
+const src = __dirname + '/src';
+
 fractal.set('project.title', 'Marigold');
 
 // use Nunjucks as the templating engine
 fractal.components.engine(require('@frctl/nunjucks')({
   filters: require('./lib/template-filters')
 }));
-
-const src = __dirname + '/src';
 
 fractal.components.set('ext', '.njk');
 fractal.components.set('path', src + '/components');
@@ -21,3 +21,14 @@ fractal.web.set('static.path', src + '/assets');
 fractal.web.set('static.mount', 'assets');
 
 fractal.web.set('builder.dest', __dirname + '/build');
+
+const sass = require('./lib/sass');
+
+// XXX detect the CLI command
+const command = process.argv[2];
+
+if (command === 'build') {
+  sass.buildDirectory(src);
+} else if (command === 'start') {
+  sass.watchDirectory(src);
+}
