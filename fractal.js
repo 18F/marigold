440a5137e4dbc8aa5f00ec93d5cccb89a38c63af
Shawn Allen <shawn.allen@gsa.gov>
2017-01-06 10:42:24 -0800

    use the white mandelbrot "skin"

--- fractal.js
+++ fractal.js
@@ -2,6 +2,12 @@
 
 const fractal = module.exports = require('@frctl/fractal').create();
 
+const theme = require('@frctl/mandelbrot')({
+  skin: 'white'
+});
+
+fractal.web.theme(theme);
+
 fractal.set('project.title', 'Marigold');
 
 fractal.components.set('path', __dirname + '/src/components');
