bb5bad1baf8bffee0723bea95d983953a46b8737
Shawn Allen <shawn.allen@gsa.gov>
2017-01-06 16:49:49 -0800

    group const statements at the top

--- fractal.js
+++ fractal.js
@@ -2,6 +2,11 @@
 
 const fractal = module.exports = require('@frctl/fractal').create();
 
+const sass = require('./lib/sass');
+
+// XXX detect the CLI command
+const command = process.argv[2];
+
 const src = __dirname + '/src';
 
 fractal.set('project.title', 'Marigold');
@@ -21,11 +26,6 @@ fractal.web.set('static.path', src + '/assets');
 fractal.web.set('static.mount', 'assets');
 
 fractal.web.set('builder.dest', __dirname + '/build');
-
-const sass = require('./lib/sass');
-
-// XXX detect the CLI command
-const command = process.argv[2];
 
 if (command === 'build') {
   sass.buildDirectory(src);
